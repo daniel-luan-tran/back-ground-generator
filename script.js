8c73f585a6fa1b3a6e14d44f59db28dd0c30ddd4
Daniel-Luan-Tran <daniel.luan.tran@gmail.com>
2020-12-28 17:04:26 +0700

    add node_modules

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var _ = require('lodash');
+console.log(_);
+array = [1,2,3,4,5]
+console.log("answer: ", _.without(array, 3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
